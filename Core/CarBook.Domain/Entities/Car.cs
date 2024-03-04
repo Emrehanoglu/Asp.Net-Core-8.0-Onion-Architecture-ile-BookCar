14662a0f3416d04c62a86bca70d601b0151b679c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-04 22:53:22 +0300

    BookCarApp #1

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities;
+
+public class Car
+{
+    public int CarId { get; set; }
+    public int BrandId { get; set; }
+    public Brand Brand { get; set; }
+    public string Model { get; set; }
+    public string CoverImageUrl { get; set; }
+    public int Km { get; set; } //kilometre sayacı
+    public string Transmission { get; set; } //vites türü
+    public byte Seat { get; set; } //koltuk sayısı
+    public byte Luggage { get; set; } //bagaj sayısı
+    public string Fuel { get; set; } //yakıt türü
+    public string BigImageUrl { get; set; } //aracın büyük görseli
+}
