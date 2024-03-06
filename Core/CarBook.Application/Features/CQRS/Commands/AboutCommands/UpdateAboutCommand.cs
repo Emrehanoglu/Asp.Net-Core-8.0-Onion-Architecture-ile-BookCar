1fc14b74e8c65365bbc6f851be5ef8333cbdd2a3
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-07 00:22:03 +0300

    BookCarApp #10

--- Core/CarBook.Application/Features/CQRS/Commands/AboutCommands/UpdateAboutCommand.cs
+++ Core/CarBook.Application/Features/CQRS/Commands/AboutCommands/UpdateAboutCommand.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//
 namespace CarBook.Application.Features.CQRS.Commands.AboutCommands;
 
 public class UpdateAboutCommand
