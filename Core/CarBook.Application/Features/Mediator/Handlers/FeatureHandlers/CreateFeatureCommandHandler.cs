70c9cc91a116921b54d83bc2eb6c4fcb08fac944
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-10 14:35:32 +0300

    BookCarApp #16

--- Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
@@ -13,6 +13,12 @@ namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers;
 public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand>
 {
 	private readonly IRepository<Feature> _repository;
+
+	public CreateFeatureCommandHandler(IRepository<Feature> repository)
+	{
+		_repository = repository;
+	}
+
 	public async Task Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
 	{
 		await _repository.CreateAsync(new Feature
---
+++ Core/CarBook.Application/Services/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Services
+{
+	public static class ServiceRegistration
+	{
+		public static void AddApplicationServices(this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof
+				(ServiceRegistration).Assembly));
+
+			//bu noktada artık IMediator 'u ctor içerisinde gectiğimiz her yerde gecerli olacak.
+			//ve controller 'lar içerisinde tek tek handler 'ların inject edilmesinin önüne gecmiş olucam
+		}
+	}
+}
