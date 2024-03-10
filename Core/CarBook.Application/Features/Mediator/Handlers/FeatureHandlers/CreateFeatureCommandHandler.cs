5ce6b46c15bb57c1293188ab9521b5e82923f2c5
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-10 14:00:52 +0300

    BookCarApp #14

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FeatureCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers;
+
+public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand>
+{
+	private readonly IRepository<Feature> _repository;
+	public async Task Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
+	{
+		await _repository.CreateAsync(new Feature
+		{
+			Name = request.Name
+		});
+	}
+}
