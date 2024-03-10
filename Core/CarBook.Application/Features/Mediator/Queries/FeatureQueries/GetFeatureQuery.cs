20a5f9aa8fbdd9b9569c8577b60c16e247af93d5
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-10 13:43:43 +0300

    BookCarApp #13

---
+++ Core/CarBook.Application/Features/Mediator/Queries/FeatureQueries/GetFeatureQuery.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Application.Features.Mediator.Results.FeatureResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+
+//liste şeklinde GetFeatureQueryResult döndürüyor olacak
+public class GetFeatureQuery : IRequest<List<GetFeatureQueryResult>>
+{
+}
