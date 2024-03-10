966215da5c20f9af8ec80c117af5726534151251
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-10 14:17:47 +0300

    BookCarApp #15

--- Core/CarBook.Application/Features/Mediator/Commands/FeatureCommands/RemoveFeatureCommand.cs
+++ Core/CarBook.Application/Features/Mediator/Commands/FeatureCommands/RemoveFeatureCommand.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace CarBook.Application.Features.Mediator.Commands.FeatureCommands;
 
 public class RemoveFeatureCommand : IRequest
-{
+{ 
     public int Id { get; set; }
 
 	public RemoveFeatureCommand(int id)
