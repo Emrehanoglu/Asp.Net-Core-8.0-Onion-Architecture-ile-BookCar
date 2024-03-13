03b7b2ea5eeb78b85f1b3508ab29019c4f95d641
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-13 20:49:51 +0300

    BookCarApp #25

---
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponent/_AboutUsComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.AboutViewComponent
+{
+    public class _AboutUsComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //api tarafında request gönderebilmek için önce istemci oluşturdum.
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7195/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //json formatta gelen veriyi okudum.
+                //ardından deserialize ile string formata çevirdim.
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponent/_TestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.TestimonialViewComponent
+{
+    public class _TestimonialComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async IViewComponentResult Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost7195/api/Testimonials");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                return values;
+            }
+            
+            return View();
+        }
+    }
+}
