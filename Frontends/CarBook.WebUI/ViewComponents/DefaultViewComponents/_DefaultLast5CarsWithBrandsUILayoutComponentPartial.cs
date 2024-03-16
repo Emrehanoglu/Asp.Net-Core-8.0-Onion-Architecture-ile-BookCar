5eff095a9e7c63a8043f7a489f8924bbb975aa68
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-16 23:16:13 +0300

    BookCarApp #29

---
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultLast5CarsWithBrandsUILayoutComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultLast5CarsWithBrandsUILayoutComponentPartial:ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultLast5CarsWithBrandsUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responsemessage = await client.GetAsync("https://localhost:7195/api/Cars/GetLast5CarsWithBrand");
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responsemessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLast5CarsWithBrandDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
