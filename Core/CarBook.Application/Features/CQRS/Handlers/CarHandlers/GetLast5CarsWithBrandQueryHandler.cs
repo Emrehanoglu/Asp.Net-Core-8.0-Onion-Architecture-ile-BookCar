63567b41912cd4f615d462ec20ad8006cd51dcc2
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-16 22:47:33 +0300

    BookCarApp #28

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/CarHandlers/GetLast5CarsWithBrandQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Application.Features.CQRS.Results.CarResults;
+using CarBook.Application.Interfaces.CarInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.CarHandlers;
+
+public class GetLast5CarsWithBrandQueryHandler
+{
+    private readonly ICarRepository _repository;
+
+    public GetLast5CarsWithBrandQueryHandler(ICarRepository repository)
+    {
+        _repository = repository;
+    }
+    public List<GetLast5CarsWithBrandQueryResult> Handle()
+    {
+        var values = _repository.GetLast5CarsListWithBrands();
+        return values.Select(x => new GetLast5CarsWithBrandQueryResult
+        {
+            BigImageUrl = x.BigImageUrl,
+            BrandId = x.BrandId,
+            BrandName = x.Brand.Name,
+            CarId = x.CarId,
+            CoverImageUrl = x.CoverImageUrl,
+            Fuel = x.Fuel,
+            Km = x.Km,
+            Luggage = x.Luggage,
+            Model = x.Model,
+            Seat = x.Seat,
+            Transmission = x.Transmission
+        }).ToList();
+    }
+}
