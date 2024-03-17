2b51fde24281b02928824293ead49e9b49984543
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-17 20:32:18 +0300

    BookCarApp #35

---
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_GetLast3BlogsWithAuthorsComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CarBook.WebUI.ViewComponents.BlogViewComponents;
+
+public class _GetLast3BlogsWithAuthorsComponentPartial:ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _GetLast3BlogsWithAuthorsComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7195/api/Blogs/GetLast3BlogsWithAuthors");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLast3BlogsWithAuthors>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
