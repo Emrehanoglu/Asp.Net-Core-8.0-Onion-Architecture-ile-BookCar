b2ca88d44bcfe5c29c9584895a119d7471adc6f0
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-18 22:47:06 +0300

    BookCarApp #42

---
+++ Core/CarBook.Application/Features/Mediator/Results/CarPricingResults/GetCarPricingWithCarQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Results.CarPricingResults;
+
+public class GetCarPricingWithCarQueryResult
+{
+    public int CarPricingId { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string CoverImageUrl{ get; set; }
+    public decimal Amount { get; set; }
+}
