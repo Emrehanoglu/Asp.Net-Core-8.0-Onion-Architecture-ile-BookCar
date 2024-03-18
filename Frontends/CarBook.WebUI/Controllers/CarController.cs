71bf0fab500148277a92fc3516b63850f3d7aaff
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-18 20:45:43 +0300

    BookCarApp #38

--- Frontends/CarBook.WebUI/Controllers/CarController.cs
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -15,6 +15,8 @@ public class CarController : Controller
 
     public async Task<IActionResult> Index()
     {
+        ViewBag.v1 = "Araçlarımız";
+        ViewBag.v2 = "Aracınızı Seçiniz";
         var client = _httpClientFactory.CreateClient();
         var responseMessage = await client.GetAsync("https://localhost:7195/api/Cars/GetCarWithBrand");
         if (responseMessage.IsSuccessStatusCode)
