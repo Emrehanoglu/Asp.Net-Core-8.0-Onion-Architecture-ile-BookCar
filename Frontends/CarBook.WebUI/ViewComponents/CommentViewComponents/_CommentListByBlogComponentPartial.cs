846ba764a26f10f28985e42968a7bbd72d0623ed
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-20 00:39:33 +0300

    BookCarApp #50

---
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.CommentViewComponents;
+
+public class _CommentListByBlogComponentPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
