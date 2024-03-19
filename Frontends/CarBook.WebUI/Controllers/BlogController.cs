3a4c6b94de8d8ce9d33715ce97c8cb8aee7ebcd6
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-19 22:05:17 +0300

    BookCarApp #45

---
+++ Frontends/CarBook.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers;
+
+public class BlogController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public BlogController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.v1 = "Bloglar";
+        ViewBag.v2 = "Yazarlarımızın Blogları";
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7195/api/Blogs/GetAllBlogsWithAuthors");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultAllBlogsWithAuthorsDto>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
