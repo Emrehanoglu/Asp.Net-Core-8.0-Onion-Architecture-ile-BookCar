2392d4f1dd864bf16e3bc18f49e76e842f553591
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-21 02:19:48 +0300

    BookCarApp #56

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/GetTagCloudByBlogIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TagCloudQueries;
+using CarBook.Application.Features.Mediator.Results.TagCloudResults;
+using CarBook.Application.Interfaces.TagCloudInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TagCloudHandlers;
+
+public class GetTagCloudByBlogIdQueryHandler : IRequestHandler<GetTagCloudByBlogIdQuery, List<GetTagCloudByBlogIdQueryResult>>
+{
+    private readonly ITagCloudRepository _tagCloudRepository;
+
+    public GetTagCloudByBlogIdQueryHandler(ITagCloudRepository tagCloudRepository)
+    {
+        _tagCloudRepository = tagCloudRepository;
+    }
+
+    public async Task<List<GetTagCloudByBlogIdQueryResult>> Handle(GetTagCloudByBlogIdQuery request, CancellationToken cancellationToken)
+    {
+        var values = _tagCloudRepository.GetTagCloudByBlodId(request.Id);
+        return values.Select(x => new GetTagCloudByBlogIdQueryResult
+        {
+            BlogId = x.BlogId,
+            TagCloudId = x.TagCloudId,
+            Title = x.Title
+        }).ToList();
+    }
+}
---
+++ Infrastructure/CarBook.Persistance/Repositories/TagCloudRepositories/TagCloudRepository.cs
@@ -0,0 +1,26 @@
+﻿using CarBook.Application.Interfaces.TagCloudInterfaces;
+using CarBook.Domain.Entities;
+using CarBook.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistance.Repositories.TagCloudRepositories;
+
+public class TagCloudRepository : ITagCloudRepository
+{
+    private readonly CarBookContext _carBookContext;
+
+    public TagCloudRepository(CarBookContext carBookContext)
+    {
+        _carBookContext = carBookContext;
+    }
+
+    public List<TagCloud> GetTagCloudByBlodId(int id)
+    {
+        var values = _carBookContext.TagClouds.Where(x => x.BlogId == id).ToList();
+        return values;
+    }
+}
