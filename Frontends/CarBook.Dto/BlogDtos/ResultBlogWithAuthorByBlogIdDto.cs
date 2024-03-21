608af738c671f847191677f62ffea0d22e57d2cf
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-21 03:56:14 +0300

    BookCarApp #59

---
+++ Frontends/CarBook.Dto/BlogDtos/ResultBlogWithAuthorByBlogIdDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.BlogDtos;
+
+public class ResultBlogWithAuthorByBlogIdDto
+{
+    public int BlogId { get; set; }
+    public int AuthorId { get; set; }
+    public string AuthorName { get; set; }
+    public string AuthorDescription { get; set; }
+    public string AuthorImage { get; set; }
+}
