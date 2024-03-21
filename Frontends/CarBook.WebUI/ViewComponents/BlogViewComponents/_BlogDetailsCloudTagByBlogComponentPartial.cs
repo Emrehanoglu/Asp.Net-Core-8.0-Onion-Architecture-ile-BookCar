40268fdbcdce249fc5824b0ddc867435d6c32120
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-21 03:01:12 +0300

    BookCarApp #57

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCloudTagByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCloudTagByBlogComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents;
 
 public class _BlogDetailsCloudTagByBlogComponentPartial:ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _BlogDetailsCloudTagByBlogComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:7195/api/TagClouds/GetTagCloudByBlogId?id=" + id);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultTagCloudByBlogIdDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
