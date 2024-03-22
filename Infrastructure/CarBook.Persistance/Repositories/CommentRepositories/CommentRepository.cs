cd696f1df4e69a1064451f39af694adf1e58b3c4
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-22 17:59:57 +0300

    BookCarApp #61

---
+++ Infrastructure/CarBook.Persistance/Repositories/CommentRepositories/CommentRepository.cs
@@ -0,0 +1,48 @@
+﻿using CarBook.Application.Features.Repository;
+using CarBook.Domain.Entities;
+using CarBook.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistance.Repositories.CommentRepositories;
+
+public class CommentRepository : IGenericRepository<Comment>
+{
+    private readonly CarBookContext _carBookContext;
+
+    public CommentRepository(CarBookContext carBookContext)
+    {
+        _carBookContext = carBookContext;
+    }
+
+    public void Create(Comment entity)
+    {
+        _carBookContext.Comments.Add(entity);
+        _carBookContext.SaveChanges();
+    }
+
+    public List<Comment> GetAll()
+    {
+        return _carBookContext.Comments.ToList();
+    }
+
+    public Comment GetById(int id)
+    {
+        return _carBookContext.Comments.Find(id);
+    }
+
+    public void Remove(Comment entity)
+    {
+        _carBookContext.Comments.Remove(entity);
+        _carBookContext.SaveChanges();
+    }
+
+    public void Update(Comment entity)
+    {
+        _carBookContext.Comments.Update(entity);
+        _carBookContext.SaveChanges();
+    }
+}
