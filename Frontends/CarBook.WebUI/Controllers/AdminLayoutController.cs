a6a3c122061ab192637bbf8d5869a10859f492f9
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-24 10:29:45 +0300

    BookCarApp #62

---
+++ Frontends/CarBook.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers;
+
+public class AdminLayoutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult AdminHeaderPartial()
+    {
+        return PartialView();
+    }
+}
