153f66935da3d310bad63b2b8d2c27c37c97141f
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-24 11:39:01 +0300

    BookCarApp #65

---
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers;
+
+public class AdminCarController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminCarController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7195/api/Cars/");
+        return View();
+    }
+}
