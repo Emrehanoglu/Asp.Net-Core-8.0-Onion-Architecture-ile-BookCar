c964211f65a0de071f90321443d289109ba412b5
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-26 00:19:31 +0300

    BookCarApp #68

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using CarBook.Dto.CarDtos;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers;
@@ -26,8 +28,22 @@ public class AdminCarController : Controller
         return View();
     }
     [HttpGet]
-    public IActionResult CreateCar()
+    public async Task<IActionResult> CreateCar()
     {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7195/api/Brands");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.BrandId.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
+        }
         return View();
     }
 }
