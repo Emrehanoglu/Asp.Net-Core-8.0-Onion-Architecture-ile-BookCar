db5bda54aaf3cb61f2c675d5f4ac232b02983eb6
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-26 00:55:06 +0300

    BookCarApp #70

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -3,6 +3,7 @@ using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers;
 
@@ -43,6 +44,19 @@ public class AdminCarController : Controller
                                                     Value = x.BrandId.ToString()
                                                 }).ToList();
             ViewBag.BrandValues = brandValues;
+        }
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(createCarDto);
+        StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+        var responseMessage = await client.PostAsync("https://localhost:7195/api/Cars",stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
         }
         return View();
     }
