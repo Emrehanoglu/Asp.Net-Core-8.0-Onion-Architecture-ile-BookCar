9245130024bb70cc94ac2d6d9d319077edf331bb
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-26 22:33:09 +0300

    BookCarApp #78

---
+++ Frontends/CarBook.WebUI/Controllers/AdminBrandController.cs
@@ -0,0 +1,84 @@
+﻿using CarBook.Dto.BrandDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Controllers;
+
+public class AdminBrandController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminBrandController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7195/api/Brands");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+    [HttpGet]
+    public IActionResult CreateBrand()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateBrand(CreateBrandDto createBrandDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(createBrandDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("https://localhost:7195/api/Brands", stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    public async Task<IActionResult> RemoveBrand(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.DeleteAsync($"https://localhost:7195/api/Brands/{id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    [HttpGet]
+    public async Task<IActionResult> UpdateBrand(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var responseMessage2 = await client.GetAsync($"https://localhost:7195/api/Brands/{id}");
+        if (responseMessage2.IsSuccessStatusCode)
+        {
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            var value2 = JsonConvert.DeserializeObject<UpdateBrandDto>(jsonData2);
+            return View(value2);
+        }
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> UpdateBrand(UpdateBrandDto updateBrandDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(updateBrandDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PutAsync("https://localhost:7195/api/Brands", stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+}
