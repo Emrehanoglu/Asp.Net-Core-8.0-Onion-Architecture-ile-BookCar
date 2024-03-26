d272740934beb19c6acccb572e7f86eb2e326ad8
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-26 21:27:53 +0300

    BookCarApp #75

---
+++ Frontends/CarBook.WebUI/Controllers/AdminFeatureController.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Dto.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers;
+
+public class AdminFeatureController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminFeatureController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7195/api/Features");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
