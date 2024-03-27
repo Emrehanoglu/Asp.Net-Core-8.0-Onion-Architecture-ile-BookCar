20e8609793e4a976b8b8a4ea96f45e559aefae96
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-03-28 01:23:34 +0300

    BookCarApp #80

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminBannerController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7195/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
